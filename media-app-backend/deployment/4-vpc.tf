f427808b8932f7f03c1a6c77e712bb4c210e9a33
mamed-gasimov <mamed.gasymov@gmail.com>
2023-02-16 13:00:01 +0400

    terraform files for vpc, subnets, igw, public route tables

---
+++ media-app-backend/deployment/4-vpc.tf
@@ -0,0 +1,10 @@
+# VPC - Virtual Private Cloud
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}" })
+  )
+}
